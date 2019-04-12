571846c58aaa8a2de5fd8f5f616dc0cd53996f8f
Svetlin Ralchev <svet@ralch.com>
2019-04-12 13:12:28 +0200

    Update the example

--- example/database/repository.go
+++ example/database/repository.go
@@ -1,8 +1,10 @@
 // Package database contains an repository of database schema ''
-// Auto-generated at Thu, 11 Apr 2019 16:37:32 CEST
+// Auto-generated at Fri, 12 Apr 2019 13:09:42 CEST
 package database
 
 import (
+	"context"
+
 	"github.com/phogolabs/orm"
 	"github.com/phogolabs/orm/example/database/model"
 )
@@ -15,10 +17,15 @@ type UserRepository struct {
 
 // SelectAll returns all User from the database
 func (r *UserRepository) SelectAll() ([]*model.User, error) {
+	return r.SelectAllContext(context.TODO())
+}
+
+// SelectAllContext returns all User from the database
+func (r *UserRepository) SelectAllContext(ctx context.Context) ([]*model.User, error) {
 	records := []*model.User{}
 	routine := orm.Routine("select-all-users")
 
-	if err := r.Gateway.Select(&records, routine); err != nil {
+	if err := r.Gateway.SelectContext(ctx, &records, routine); err != nil {
 		return nil, err
 	}
 
@@ -27,6 +34,11 @@ func (r *UserRepository) SelectAll() ([]*model.User, error) {
 
 // SelectByPK returns a record of User for given primary key
 func (r *UserRepository) SelectByPK(id int) (*model.User, error) {
+	return r.SelectByPKContext(context.TODO(), id)
+}
+
+// SelectByPKContext returns a record of User for given primary key
+func (r *UserRepository) SelectByPKContext(ctx context.Context, id int) (*model.User, error) {
 	param := orm.Map{
 		"id": id,
 	}
@@ -34,7 +46,7 @@ func (r *UserRepository) SelectByPK(id int) (*model.User, error) {
 	routine := orm.Routine("select-user-by-pk", param)
 	record := &model.User{}
 
-	if err := r.Gateway.SelectOne(record, routine); err != nil {
+	if err := r.Gateway.SelectOneContext(ctx, record, routine); err != nil {
 		return nil, err
 	}
 
@@ -43,10 +55,15 @@ func (r *UserRepository) SelectByPK(id int) (*model.User, error) {
 
 // SearchAll returns all  from the database for given RQL query
 func (r *UserRepository) SearchAll(query *orm.RQLQuery) ([]*model.User, error) {
+	return r.SearchAllContext(context.TODO(), query)
+}
+
+// SearchAllContext returns all  from the database for given RQL query
+func (r *UserRepository) SearchAllContext(ctx context.Context, query *orm.RQLQuery) ([]*model.User, error) {
 	records := []*model.User{}
 	routine := orm.RQL("users", query)
 
-	if err := r.Gateway.Select(&records, routine); err != nil {
+	if err := r.Gateway.SelectContext(ctx, &records, routine); err != nil {
 		return nil, err
 	}
 
@@ -55,25 +72,40 @@ func (r *UserRepository) SearchAll(query *orm.RQLQuery) ([]*model.User, error) {
 
 // Insert inserts a record of type User into the database
 func (r *UserRepository) Insert(row *model.User) error {
+	return r.InsertContext(context.TODO(), row)
+}
+
+// InsertContext inserts a record of type User into the database
+func (r *UserRepository) InsertContext(ctx context.Context, row *model.User) error {
 	routine := orm.Routine("insert-user", row)
-	_, err := r.Gateway.Exec(routine)
+	_, err := r.Gateway.ExecContext(ctx, routine)
 	return err
 }
 
 // UpdateByPK updates a record of type User for given primary key
 func (r *UserRepository) UpdateByPK(row *model.User) error {
+	return r.UpdateByPKContext(context.TODO(), row)
+}
+
+// UpdateByPKContext updates a record of type User for given primary key
+func (r *UserRepository) UpdateByPKContext(ctx context.Context, row *model.User) error {
 	routine := orm.Routine("update-user-by-pk", row)
-	_, err := r.Gateway.Exec(routine)
+	_, err := r.Gateway.ExecContext(ctx, routine)
 	return err
 }
 
 // DeleteByPK deletes a record of User for given primary key
 func (r *UserRepository) DeleteByPK(id int) error {
+	return r.DeleteByPKContext(context.TODO(), id)
+}
+
+// DeleteByPKContext deletes a record of User for given primary key
+func (r *UserRepository) DeleteByPKContext(ctx context.Context, id int) error {
 	param := orm.Map{
 		"id": id,
 	}
 
 	routine := orm.Routine("delete-user-by-pk", param)
-	_, err := r.Gateway.Exec(routine)
+	_, err := r.Gateway.ExecContext(ctx, routine)
 	return err
 }
