fb383b230a747311cbfa610a687d09c0c751b934
Svetlin Ralchev <svet@ralch.com>
2019-10-07 17:09:34 +0300

    Allo setting of db connection
    - Add SetMaxIdleConns
    - Add SetMaxOpenConns
    - Add SetConnMaxLifetime

--- gateway.go
+++ gateway.go
@@ -3,6 +3,7 @@ package orm
 import (
 	"context"
 	"io"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/phogolabs/prana"
@@ -33,6 +34,9 @@ func Open(driver, source string) (*Gateway, error) {
 		return nil, err
 	}
 
+	db.SetMaxIdleConns(32)
+	db.SetMaxOpenConns(32)
+
 	return &Gateway{
 		provider: &sqlexec.Provider{DriverName: driver},
 		db:       db,
@@ -52,6 +56,41 @@ func (g *Gateway) DriverName() string {
 // Ping pins the underlying database
 func (g *Gateway) Ping() error {
 	return g.db.Ping()
+}
+
+// SetMaxIdleConns sets the maximum number of connections in the idle
+// connection pool.
+//
+// If MaxOpenConns is greater than 0 but less than the new MaxIdleConns,
+// then the new MaxIdleConns will be reduced to match the MaxOpenConns limit.
+//
+// If n <= 0, no idle connections are retained.
+//
+// The default max idle connections is currently 2. This may change in
+// a future release.
+func (g *Gateway) SetMaxIdleConns(value int) {
+	g.db.SetMaxIdleConns(value)
+}
+
+// SetMaxOpenConns sets the maximum number of open connections to the database.
+//
+// If MaxIdleConns is greater than 0 and the new MaxOpenConns is less than
+// MaxIdleConns, then MaxIdleConns will be reduced to match the new
+// MaxOpenConns limit.
+//
+// If n <= 0, then there is no limit on the number of open connections.
+// The default is 0 (unlimited).
+func (g *Gateway) SetMaxOpenConns(value int) {
+	g.db.SetMaxOpenConns(value)
+}
+
+// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+//
+// Expired connections may be closed lazily before reuse.
+//
+// If d <= 0, connections are reused forever.
+func (g *Gateway) SetConnMaxLifetime(duration time.Duration) {
+	g.db.SetConnMaxLifetime(duration)
 }
 
 // PingContext pins the underlying database
