95c33310ff73cc0106460bf86e7203cb22d11847
Svetlin Ralchev <svetlin@ralch.com>
2020-11-07 08:42:30 +0000

    Improve logging

--- dialect/dialect.go
+++ dialect/dialect.go
@@ -7,7 +7,6 @@ package dialect
 import (
 	"context"
 	"database/sql/driver"
-	"time"
 )
 
 // Dialect names for external usage.
@@ -57,81 +56,4 @@ func (nopTx) Rollback() error { return nil }
 // the provided Driver d.
 func NopTx(d Driver) Tx {
 	return nopTx{d}
-}
-
-// Logger represents a logger
-type Logger interface {
-	Debugf(string, ...interface{})
-	Infof(string, ...interface{})
-}
-
-// LoggerDriver is a driver that logs all driver operations.
-type LoggerDriver struct {
-	Driver
-	logger Logger
-}
-
-// Log gets a driver and an optional logging function, and returns
-// a new debugged-driver that prints all outgoing operations.
-func Log(d Driver, logger Logger) Driver {
-	return &LoggerDriver{d, logger}
-}
-
-// Exec logs its params and calls the underlying driver Exec method.
-func (d *LoggerDriver) Exec(ctx context.Context, query string, args, v interface{}) error {
-	d.logger.Infof("driver.Query: query=%v", query)
-	d.logger.Debugf("driver.Query: args=%v", args)
-	return d.Driver.Exec(ctx, query, args, v)
-}
-
-// Query logs its params and calls the underlying driver Query method.
-func (d *LoggerDriver) Query(ctx context.Context, query string, args, v interface{}) error {
-	d.logger.Infof("driver.Query: query=%v", query)
-	d.logger.Debugf("driver.Query: args=%v", args)
-	return d.Driver.Query(ctx, query, args, v)
-}
-
-// Tx adds an log-id for the transaction and calls the underlying driver Tx command.
-func (d *LoggerDriver) Tx(ctx context.Context) (Tx, error) {
-	tx, err := d.Driver.Tx(ctx)
-	if err != nil {
-		return nil, err
-	}
-	id := time.Now().Unix()
-	d.logger.Infof("driver.Tx(%d): started", id)
-	return &LoggerTx{tx, id, d.logger, ctx}, nil
-}
-
-// LoggerTx is a transaction implementation that logs all transaction operations.
-type LoggerTx struct {
-	Tx                     // underlying transaction.
-	id     int64           // transaction logging id.
-	logger Logger          // log function. defaults to fmt.Println.
-	ctx    context.Context // underlying transaction context.
-}
-
-// Exec logs its params and calls the underlying transaction Exec method.
-func (d *LoggerTx) Exec(ctx context.Context, query string, args, v interface{}) error {
-	d.logger.Infof("Tx(%d).Exec: query=%v args=%v", d.id, query)
-	d.logger.Debugf("Tx(%d).Exec: args=%v", d.id, args)
-	return d.Tx.Exec(ctx, query, args, v)
-}
-
-// Query logs its params and calls the underlying transaction Query method.
-func (d *LoggerTx) Query(ctx context.Context, query string, args, v interface{}) error {
-	d.logger.Infof("Tx(%d).Query: query=%v", d.id, query)
-	d.logger.Debugf("Tx(%d).Query: args=%v", args)
-	return d.Tx.Query(ctx, query, args, v)
-}
-
-// Commit logs this step and calls the underlying transaction Commit method.
-func (d *LoggerTx) Commit() error {
-	d.logger.Infof("Tx(%d): committed", d.id)
-	return d.Tx.Commit()
-}
-
-// Rollback logs this step and calls the underlying transaction Rollback method.
-func (d *LoggerTx) Rollback() error {
-	d.logger.Infof("Tx(%d): rollbacked", d.id)
-	return d.Tx.Rollback()
 }
---
+++ dialect/logger.go
@@ -0,0 +1,172 @@
+package dialect
+
+import (
+	"context"
+	"time"
+)
+
+// Logger represents a logger
+type Logger interface {
+	Debugf(string, ...interface{})
+	Errorf(string, ...interface{})
+	Infof(string, ...interface{})
+
+	WithFields(map[string]interface{}) Logger
+	WithField(string, value interface{}) Logger
+	WithError(error) Logger
+}
+
+// LoggerDriver is a driver that logs all driver operations.
+type LoggerDriver struct {
+	Driver
+	logger Logger
+}
+
+// Log gets a driver and an optional logging function, and returns
+// a new debugged-driver that prints all outgoing operations.
+func Log(d Driver, logger Logger) Driver {
+	return &LoggerDriver{d, logger}
+}
+
+// Exec logs its params and calls the underlying driver Exec method.
+func (d *LoggerDriver) Exec(ctx context.Context, query string, args, v interface{}) error {
+	var (
+		start  = time.Now()
+		logger = d.logger
+	)
+
+	err := d.Driver.Exec(ctx, query, args, v)
+
+	logger = logger.WithField("sql.query", query)
+	logger = logger.WithField("sql.param", args)
+	logger = logger.WithField("sql.duration", time.Now().Sub(start))
+
+	if err != nil {
+		logger.WithError(err).Errorf("query.exec fail")
+		return err
+	}
+
+	logger.Infof("query.exec success")
+	return nil
+}
+
+// Query logs its params and calls the underlying driver Query method.
+func (d *LoggerDriver) Query(ctx context.Context, query string, args, v interface{}) error {
+	var (
+		start  = time.Now()
+		logger = d.logger
+	)
+
+	err := d.Driver.Query(ctx, query, args, v)
+
+	logger = logger.WithField("sql.query", query)
+	logger = logger.WithField("sql.param", args)
+	logger = logger.WithField("sql.duration", time.Now().Sub(start))
+
+	if err != nil {
+		logger.WithError(err).Errorf("query.exec fail")
+		return err
+	}
+
+	logger.Infof("query.exec success")
+	return nil
+}
+
+// Tx adds an log-id for the transaction and calls the underlying driver Tx command.
+func (d *LoggerDriver) Tx(ctx context.Context) (Tx, error) {
+	logger := d.logger.WithField("sql.tx", time.Now().Unix())
+
+	tx, err := d.Driver.Tx(ctx)
+	if err != nil {
+		logger.WithError(err).Errorf("tx.start fail")
+		return nil, err
+	}
+
+	logger.Infof("tx.start success")
+	return &LoggerTx{tx, logger, ctx}, nil
+}
+
+// LoggerTx is a transaction implementation that logs all transaction operations.
+type LoggerTx struct {
+	Tx                     // underlying transaction.
+	logger Logger          // log function. defaults to fmt.Println.
+	ctx    context.Context // underlying transaction context.
+}
+
+// Exec logs its params and calls the underlying transaction Exec method.
+func (d *LoggerTx) Exec(ctx context.Context, query string, args, v interface{}) error {
+	var (
+		start  = time.Now()
+		logger = d.logger
+	)
+
+	err := d.Tx.Exec(ctx, query, args, v)
+
+	logger = logger.WithField("sql.query", query)
+	logger = logger.WithField("sql.param", args)
+	logger = logger.WithField("sql.duration", time.Now().Sub(start))
+
+	if err != nil {
+		logger.WithError(err).Errorf("query.exec fail")
+		return err
+	}
+
+	logger.Infof("query.exec success")
+	return nil
+}
+
+// Query logs its params and calls the underlying transaction Query method.
+func (d *LoggerTx) Query(ctx context.Context, query string, args, v interface{}) error {
+	var (
+		start  = time.Now()
+		logger = d.logger
+	)
+
+	err := d.Tx.Query(ctx, query, args, v)
+
+	logger = logger.WithField("sql.query", query)
+	logger = logger.WithField("sql.param", args)
+	logger = logger.WithField("sql.duration", time.Now().Sub(start))
+
+	if err != nil {
+		logger.WithError(err).Errorf("query.exec fail")
+		return err
+	}
+
+	logger.Infof("query.exec success")
+	return nil
+}
+
+// Commit logs this step and calls the underlying transaction Commit method.
+func (d *LoggerTx) Commit() error {
+	var (
+		logger = d.logger
+		err    = d.Tx.Commit()
+	)
+
+	if err != nil {
+		logger = d.logger.WithError(err)
+		logger.Errorf("tx.commit fail")
+		return err
+	}
+
+	logger.Infof("tx.commit success")
+	return nil
+}
+
+// Rollback logs this step and calls the underlying transaction Rollback method.
+func (d *LoggerTx) Rollback() error {
+	var (
+		logger = d.logger
+		err    = d.Tx.Rollback()
+	)
+
+	if err != nil {
+		logger = d.logger.WithError(err)
+		logger.Errorf("tx.rollback fail")
+		return err
+	}
+
+	logger.Infof("tx.rollback success")
+	return nil
+}
